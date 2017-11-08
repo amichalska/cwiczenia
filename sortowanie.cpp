e0c0692231b69e8bb10a382ce0dce61137a6adde
Agnieszka Michalska <32893298+amichalska@users.noreply.github.com>
2017-11-08 09:09:44 +0100

    Add files via upload
    Sortowanie z dodatkowym warunkiem stopu

---
+++ sortowanie.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void sortowanie_babelkowe(int tab[],int n)
+{  
+  bool is_sorted;
+  for(int i=0;i<n;i++)
+    for(int j=1;j<n-i;j++)
+    {if(tab[j-1]>tab[j])
+      swap(tab[j-1], tab[j]);
+      is_sorted=false;
+      if(true==is_sorted) break;
+     } 
+}
+
+int main()
+    {
+		
+		int *tab, n;
+ 
+  cout<<"Ilosc liczb do posortowania: ";
+  cin>>n;
+ 
+  tab = new int [n];
+  for(int i=0;i<n;i++)
+    cin>>tab[i];
+ 
+  sortowanie_babelkowe(tab,n);
+ 
+  for(int i=0;i<n;i++)
+          cout<<tab[i]<<" ";
+ 
+  cout<<endl;
+  return 0;
+		
+    
+   }
